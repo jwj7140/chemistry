4091e9d393ec887dbc99f6cd580b0a7f7ee2d1ae
jwj7140 <32943891+jwj7140@users.noreply.github.com>
2019-09-01 23:56:07 +0900

    메뉴 토대 구현

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,19 @@
 window.addEventListener('load', function() {
+  // var s = true;
   var menu = document.querySelector('.menu_btn');
   console.log(menu);
+  menu.addEventListener('click', function () {
+    var menu = document.querySelector('#menu_in');
+    menu.classList.toggle('off');
+    menu.classList.toggle('on');
+    // if (s) {
+    //   menu.classList.remove('off');
+    //   menu.classList.add('on');
+    //   s = false;
+    // } else {
+    //   menu.classList.remove('on');
+    //   menu.classList.add('off');
+    //   s = true;
+    // }
+  });
 });
