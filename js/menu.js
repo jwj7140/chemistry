4d40d8b03178e3364696c8e235e5403cbce7470c
jwj7140 <32943891+jwj7140@users.noreply.github.com>
2019-09-03 00:23:12 +0900

    1차 템플릿

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,34 @@
 window.addEventListener('load', function() {
   // var s = true;
-  var menu = document.querySelector('.menu_btn');
-  console.log(menu);
-  menu.addEventListener('click', function () {
-    var menu = document.querySelector('#menu_in');
+  var menu_o = document.querySelector('.menu_o');
+  var menu_c = document.querySelector('.menu_c');
+  var menu = document.querySelector('#menu_in');
+
+  menu_o.addEventListener('click', function () {
     menu.classList.toggle('off');
     menu.classList.toggle('on');
+    menu_o.classList.toggle('off');
+    menu_o.classList.toggle('on');
+    menu_c.classList.toggle('on');
+    menu_c.classList.toggle('off');
+    // if (s) {
+    //   menu.classList.remove('off');
+    //   menu.classList.add('on');
+    //   s = false;
+    // } else {
+    //   menu.classList.remove('on');
+    //   menu.classList.add('off');
+    //   s = true;
+    // }
+  });
+
+  menu_c.addEventListener('click', function () {
+    menu.classList.toggle('off');
+    menu.classList.toggle('on');
+    menu_o.classList.toggle('off');
+    menu_o.classList.toggle('on');
+    menu_c.classList.toggle('on');
+    menu_c.classList.toggle('off');
     // if (s) {
     //   menu.classList.remove('off');
     //   menu.classList.add('on');
