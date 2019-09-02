191c500cab606fab69a226a0a5132d0a29f46a4b
정우준 <jwj7140@naver.com>
2019-09-02 15:08:25 +0000

    1차 템플릿

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,24 @@
 window.addEventListener('load', function() {
-  var menu = document.querySelector('.menu_btn');
-  console.log(menu);
+  // var s = true;
+  var menu_o = document.querySelector('.menu_o');
+  var menu_c = document.querySelector('.menu_c');
+  var menu = document.querySelector('#menu_in');
+
+  menu_o.addEventListener('click', function () {
+    menu.classList.toggle('off');
+    menu.classList.toggle('on');
+    menu_o.classList.toggle('off');
+    menu_o.classList.toggle('on');
+    menu_c.classList.toggle('on');
+    menu_c.classList.toggle('off');
+  });
+
+  menu_c.addEventListener('click', function () {
+    menu.classList.toggle('off');
+    menu.classList.toggle('on');
+    menu_o.classList.toggle('off');
+    menu_o.classList.toggle('on');
+    menu_c.classList.toggle('on');
+    menu_c.classList.toggle('off');
+  });
 });
