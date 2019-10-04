d23861bc47d7fe21979fae858135868680760b86
정우준 <jwj7140@naver.com>
2019-10-04 17:47:34 +0900

    y,x 충돌구현 1

--- static/js/gravity.js
+++ static/js/gravity.js
@@ -1,6 +1,6 @@
 var falls=Array();
 function gravity(n) {
-  var crashy = 712;
+  var crash = [712, 0];
   var a;
   var b;
   falls[n] = 0;
@@ -8,15 +8,13 @@ function gravity(n) {
   var gravity = 5;
   setInterval (function() {
     if (move[n] == 0) {
-      if (sprite[n].height+sprite[n].y>crashy) {
-        sprite[n].y = crashy-sprite[n].height+1;
+      if (sprite[n].height+sprite[n].y>crash[0]) {
+        sprite[n].y = crash[0]-sprite[n].height+1;
         falls[n]=0;
       } else {
         falls[n] = Math.min(10000, Math.max(-10000, falls[n] + gravity * delta / 100));
         sprite[n].y += falls[n];
       }
-    } else {
-      // crashy = 682;
     }
     b=-1;
     for (a=0; a<sprite.length; a++) {
@@ -24,11 +22,19 @@ function gravity(n) {
         if (sprite[n].y+sprite[n].height > sprite[a].y && sprite[n].y < sprite[a].y) {
           if (n != a) {
             b=a;
-            crashy = sprite[a].y;
-            // if (move[n] != 0) {
-            //   sprite[n].y = crashy-sprite[n].height+1;
-            // }
-            console.log (n);
+            crash[0] = sprite[a].y;
+            break;
+          }
+        } else if (sprite[n].y-sprite[a].y < sprite[a].height-2 && sprite[n].y-sprite[a].y > -1*sprite[n].height+2) {
+          if (n != a) {
+            if (move[n] == 0) {
+              if (sprite[n].x > sprite[a].x) {        //왼쪽에서 밀림
+                sprite[n].x = sprite[a].x+sprite[a].width;
+              } else if (sprite[n].x < sprite[a].x) {   //오른쪽에서 밀림
+                sprite[n].x = sprite[a].x-sprite[n].width;
+              }
+            }
+            console.log (n+"yp");
             break;
           }
         }
@@ -36,8 +42,7 @@ function gravity(n) {
     }
     if (b != -1) {
     } else {
-      crashy = 712;
+      crash[0] = 712;
     }
-    console.log(crashy);
   }, 50)
 }
